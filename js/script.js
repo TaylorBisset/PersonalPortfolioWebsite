535e7de42ef29303db5d84df7ccef4c017796747
Taylor Bisset <taylorbisset@gmail.com>
2025-02-22 15:53:19 -0800

    Added dark light button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 // ============================= //
 // - - - - - script.js - - - - - //
 // ============================= //
+
+const themeToggle = document.getElementById('theme-toggle');
+
+// Check if a theme is already set in localStorage
+if (localStorage.getItem('theme') === 'light') {
+    document.documentElement.classList.add('light-theme');
+}
+
+// Toggle theme on button click
+themeToggle.addEventListener('click', () => {
+    document.documentElement.classList.toggle('light-theme');
+
+    // Store the user's preference in localStorage
+    if (document.documentElement.classList.contains('light-theme')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
